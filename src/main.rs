2f846fcaf0bfbf0f95802777ed1d07910fa36c8d
Satyam Lal <ssatyamlal@gmail.com>
2026-02-18 16:57:37 +0530

    rollover to single main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use std::{
-    thread,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
     sync::{
         atomic::{AtomicUsize, Ordering},
-        Arc
-    }
+        Arc,
+    },
+    thread,
 };
 
-mod resp;
+#[derive(Debug)]
+enum RespValue {
+    SimpleString(String),
+}
 
-use resp::RespValue;
+impl RespValue {
+    fn serialize(&self) -> Vec<u8> {
+        match self {
+            RespValue::SimpleString(s) => {
+                let mut bytes = Vec::new();
+                bytes.push(b'+');
+                bytes.extend_from_slice(s.as_bytes());
+                bytes.push(b'\r');
+                bytes.push(b'\n');
+                
+                bytes
+            }
+        }
+    }
+}
 
 struct ServerState {
     active_users: AtomicUsize,
@@ -18,30 +35,27 @@ struct ServerState {
 
 fn handle_client(mut stream: TcpStream, user_id: usize, state: Arc<ServerState>) {
     let mut buffer = [0; 512];
+    
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => {
-                let current_count = state.active_users.fetch_sub(1, Ordering::SeqCst) - 1;
-                println!("User {} connected. Active Users: {}", user_id, current_count);
+                state.active_users.fetch_sub(1, Ordering::SeqCst);
+                let current_count = state.active_users.load(Ordering::SeqCst);
+                println!("User {} disconnected. Active Users: {}", user_id, current_count);
                 break;
             }
-            Ok(_bytes_read) => {  // change bytes_read --> _bytes_read to ignore the size for now, blindly replying "OK"
+            Ok(_bytes_read) => {
                 let response = RespValue::SimpleString("OK".to_string());
-
-                // We call the function we wrote in resp.rs to get the protocol bytes (+OK\r\n) and then 
-                // us object ko bytes mein convert kiya (0x2B 0x4F 0x4B 0x0D 0x0A).
-                let encoded_bytes = response.encode();
-
-                // Send Over TCP. Write those specific bytes to the stream.
-                // This pushes the byte buffer into the kernel's TCP socket buffer.
-                if let Err(e) = stream.write_all(&encoded_bytes)  {
+                let encoded_response = response.serialize();
+                
+                if let Err(e) = stream.write_all(&encoded_response) {
                     eprintln!("Write error for user {}: {}", user_id, e);
                     break;
                 }
             }
             Err(_e) => {
-                let current_count = state.active_users.fetch_sub(1, Ordering::SeqCst) - 1;
-                println!("User {} dropped. Active Users: {}", user_id, current_count);
+                state.active_users.fetch_sub(1, Ordering::SeqCst);
+                println!("User {} dropped error.", user_id);
                 break;
             }
         }
@@ -49,9 +63,9 @@ fn handle_client(mut stream: TcpStream, user_id: usize, state: Arc<ServerState>)
 }
 
 fn main() -> std::io::Result<()> {
-    let localhost = "127.0.0.1:9999";
+    let localhost = "127.0.0.1:6379"; 
     let listener = TcpListener::bind(localhost)?;
-    println!("Concurrent Server is listening on {}", localhost);
+    println!("ARCDB Server (Redis Compatible) listening on {}", localhost);
 
     let state = Arc::new(ServerState {
         active_users: AtomicUsize::new(0),
@@ -63,11 +77,14 @@ fn main() -> std::io::Result<()> {
             Ok(stream) => {
                 unique_id_counter += 1;
                 let state_clone = Arc::clone(&state);
-                let count = state.active_users.fetch_add(1, Ordering::SeqCst) + 1;
+                state.active_users.fetch_add(1, Ordering::SeqCst);
+                let count = state.active_users.load(Ordering::SeqCst);
 
                 println!("User {} connected. Total users: {}", unique_id_counter, count);
 
-                thread::spawn(move || {handle_client(stream, unique_id_counter, state_clone);});
+                thread::spawn(move || {
+                    handle_client(stream, unique_id_counter, state_clone);
+                });
             }
             Err(e) => {
                 eprintln!("Connection failed: {}", e);
